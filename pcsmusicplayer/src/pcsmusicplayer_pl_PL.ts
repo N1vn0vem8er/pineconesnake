e98f150998b4d4aa6869c6989673283a537d2ecb
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-05-19 23:01:01 +0200

    Started adding translation

--- pcsmusicplayer/src/pcsmusicplayer_pl_PL.ts
+++ pcsmusicplayer/src/pcsmusicplayer_pl_PL.ts
@@ -1,3 +1,368 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pl_PL"></TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AlbumsWidget</name>
+    <message>
+        <location filename="albumswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="albumswidget.ui" line="42"/>
+        <source>Back</source>
+        <translation>Powrót</translation>
+    </message>
+</context>
+<context>
+    <name>AllTracksWidget</name>
+    <message>
+        <location filename="alltrackswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="alltrackswidget.ui" line="38"/>
+        <source>Sort</source>
+        <translation>Sortój</translation>
+    </message>
+    <message>
+        <location filename="alltrackswidget.ui" line="46"/>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <location filename="alltrackswidget.ui" line="51"/>
+        <source>Title</source>
+        <translation>Tytuł</translation>
+    </message>
+    <message>
+        <location filename="alltrackswidget.ui" line="56"/>
+        <source>Artist</source>
+        <translation>Artysta</translation>
+    </message>
+    <message>
+        <location filename="alltrackswidget.ui" line="61"/>
+        <source>Times Played</source>
+        <translation>Odtworzono</translation>
+    </message>
+    <message>
+        <location filename="alltrackswidget.ui" line="72"/>
+        <source>Search</source>
+        <translation>Szukaj</translation>
+    </message>
+</context>
+<context>
+    <name>ArtistsWidget</name>
+    <message>
+        <location filename="artistswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="artistswidget.ui" line="42"/>
+        <source>Back</source>
+        <translation>Powrót</translation>
+    </message>
+</context>
+<context>
+    <name>FavoriteWidget</name>
+    <message>
+        <location filename="favoritewidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>FilesWidget</name>
+    <message>
+        <location filename="fileswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="fileswidget.ui" line="38"/>
+        <source>Sort</source>
+        <translation>Sortój</translation>
+    </message>
+    <message>
+        <location filename="fileswidget.ui" line="46"/>
+        <source>Title</source>
+        <translation>Tytuł</translation>
+    </message>
+    <message>
+        <location filename="fileswidget.ui" line="51"/>
+        <source>Times played</source>
+        <translation>Odtworzono</translation>
+    </message>
+    <message>
+        <location filename="fileswidget.ui" line="56"/>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <location filename="fileswidget.ui" line="61"/>
+        <source>Artist</source>
+        <translation>Artysta</translation>
+    </message>
+    <message>
+        <location filename="fileswidget.ui" line="66"/>
+        <source>File name</source>
+        <translation>Nazwa pliku</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>PCS Music Player</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <source>Now Playing</source>
+        <translation>Odtwarzane</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="43"/>
+        <source>Favorites</source>
+        <translation>Ulubione</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="48"/>
+        <source>Playlists</source>
+        <translation>Playlisty</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <source>Albums</source>
+        <translation>Albumy</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="58"/>
+        <source>Artists</source>
+        <translation>Artyści</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>All Track</source>
+        <translation>Wszystkie Utwory</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <source>Files</source>
+        <translation>Pliki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>About</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="115"/>
+        <source>Open Track</source>
+        <translation>Otwórz Utwór</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="123"/>
+        <source>Exit</source>
+        <translation>Zakończ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <location filename="mainwindow.cpp" line="92"/>
+        <source>About PCS Music Player</source>
+        <translation>A PCS Music Player</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="139"/>
+        <location filename="mainwindow.cpp" line="43"/>
+        <source>About Qt</source>
+        <translation>O Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="92"/>
+        <source>&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Music Player&lt;/h3&gt;&lt;p&gt;PCS Music Player is a simple music player. It is a part of the Pinecone Snake project.&lt;/p&gt;&lt;p&gt;Version: %1&lt;/p&gt;&lt;p&gt;License: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Music Player&lt;/h3&gt;&lt;p&gt;PCS Music Player jest prostym odtwarzaczem muzyki. Jest częścią projektu Pinecone Snake.&lt;/p&gt;&lt;p&gt;Wersja: %1&lt;/p&gt;&lt;p&gt;Licencja: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>Open track</source>
+        <translation>Otwórz Utwór</translation>
+    </message>
+</context>
+<context>
+    <name>NowPlayingWidget</name>
+    <message>
+        <location filename="nowplayingwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="nowplayingwidget.ui" line="36"/>
+        <source>Not playing</source>
+        <translation>Nie odtwarza</translation>
+    </message>
+    <message>
+        <location filename="nowplayingwidget.ui" line="114"/>
+        <source>Make Favorite</source>
+        <translation>Dodaj do ulubionych</translation>
+    </message>
+    <message>
+        <location filename="nowplayingwidget.cpp" line="26"/>
+        <source>Favorite</source>
+        <translation>Ulubione</translation>
+    </message>
+    <message>
+        <location filename="nowplayingwidget.cpp" line="26"/>
+        <source>Not favorite</source>
+        <translation>Nie ulubione</translation>
+    </message>
+</context>
+<context>
+    <name>PlayingWidget</name>
+    <message>
+        <location filename="playingwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="23"/>
+        <source>Not playing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="30"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="43"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="56"/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="69"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="82"/>
+        <location filename="playingwidget.ui" line="96"/>
+        <source>00:00:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playingwidget.ui" line="103"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistNameDialog</name>
+    <message>
+        <location filename="playlistnamedialog.ui" line="14"/>
+        <source>Playlist Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistnamedialog.ui" line="23"/>
+        <source>Enter name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistWidget</name>
+    <message>
+        <location filename="playlistwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistwidget.ui" line="25"/>
+        <source>Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistwidget.ui" line="45"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistwidget.ui" line="58"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistwidget.ui" line="71"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistwidget.cpp" line="170"/>
+        <source>Critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistwidget.cpp" line="170"/>
+        <source>Playlist with that name exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistsWidget</name>
+    <message>
+        <location filename="playlistswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistswidget.ui" line="42"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistswidget.cpp" line="92"/>
+        <source>Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="playlistswidget.cpp" line="92"/>
+        <source>Delete playlist?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="settingswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswidget.ui" line="23"/>
+        <source>Search directories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswidget.ui" line="32"/>
+        <source>Separate with ;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingswidget.ui" line="42"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
