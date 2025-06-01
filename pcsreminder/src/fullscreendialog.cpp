ddafdee7db918da34db5dff97ece6752a973f68e
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-06-01 17:01:59 +0200

    Added constructors to fullscreendialog

--- pcsreminder/src/fullscreendialog.cpp
+++ pcsreminder/src/fullscreendialog.cpp
@@ -1,14 +1,50 @@
 #include "fullscreendialog.h"
 #include "ui_fullscreendialog.h"
 
-FullScreenDialog::FullScreenDialog(QWidget *parent)
+FullScreenDialog::FullScreenDialog(const EventManager::Event &event, QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::FullScreenDialog)
 {
     ui->setupUi(this);
+    this->event = event;
+    type = 1;
+    init();
+}
+
+FullScreenDialog::FullScreenDialog(const EventManager::RepeatedEvent &event, QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::FullScreenDialog)
+{
+    ui->setupUi(this);
+    this->repeated = event;
+    type = 0;
+    init();
 }
 
 FullScreenDialog::~FullScreenDialog()
 {
     delete ui;
+}
+
+void FullScreenDialog::init()
+{
+    connect(ui->closeButton, &QPushButton::clicked, this, &QDialog::accept);
+    switch(type)
+    {
+    case 0:
+        ui->titleLabel->setText(repeated.title);
+        ui->contentLabel->setText(repeated.content);
+        ui->timeLabel->setText(tr("Every %1 seconds").arg(repeated.everySeconds));
+        break;
+    case 1:
+        ui->titleLabel->setText(event.title);
+        ui->contentLabel->setText(event.content);
+        ui->timeLabel->setText(event.date);
+        break;
+    }
+}
+
+void FullScreenDialog::disable()
+{
+
 }
--- pcsreminder/src/fullscreendialog.h
+++ pcsreminder/src/fullscreendialog.h
@@ -1,6 +1,8 @@
 #ifndef FULLSCREENDIALOG_H
 #define FULLSCREENDIALOG_H
 
+#include "eventmanager.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +14,19 @@ class FullScreenDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit FullScreenDialog(QWidget *parent = nullptr);
+    explicit FullScreenDialog(const EventManager::Event& event, QWidget *parent = nullptr);
+    explicit FullScreenDialog(const EventManager::RepeatedEvent& event, QWidget *parent = nullptr);
     ~FullScreenDialog();
 
 private:
     Ui::FullScreenDialog *ui;
+    EventManager::Event event;
+    EventManager::RepeatedEvent repeated;
+    int type;
+    void init();
+
+private slots:
+    void disable();
 };
 
 #endif // FULLSCREENDIALOG_H
