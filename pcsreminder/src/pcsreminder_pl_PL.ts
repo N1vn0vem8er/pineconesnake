f1c57c8ab350d40ff57a0b18cedae18ace737322
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-06-07 21:10:48 +0200

    Added translation

--- pcsreminder/src/pcsreminder_pl_PL.ts
+++ pcsreminder/src/pcsreminder_pl_PL.ts
@@ -1,3 +1,444 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="pl_PL"></TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>CreateEventDialog</name>
+    <message>
+        <location filename="createeventdialog.ui" line="14"/>
+        <source>New</source>
+        <translation>Nowy</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="25"/>
+        <source>Title</source>
+        <translation>Tytuł</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="35"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="45"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="53"/>
+        <source>Repeating reminder</source>
+        <translation>Powtarzające przypomnienie</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="58"/>
+        <source>Reminder</source>
+        <translation>Przypomnienie</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="66"/>
+        <source>Repeat Every</source>
+        <translation>Powtarzaj co</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="75"/>
+        <source>Hours</source>
+        <translation>Godziny</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="82"/>
+        <location filename="createeventdialog.ui" line="99"/>
+        <source>99999999</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="85"/>
+        <location filename="createeventdialog.ui" line="102"/>
+        <location filename="createeventdialog.ui" line="119"/>
+        <source>0</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="92"/>
+        <source>Minutes</source>
+        <translation>Minuty</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="109"/>
+        <source>Seconds</source>
+        <translation>Sekundy</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="116"/>
+        <source>9999999999</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="128"/>
+        <source>Time Date</source>
+        <translation>Data godzina</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="138"/>
+        <source>Show as</source>
+        <translation>Pokaż jako</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="146"/>
+        <source>Notification</source>
+        <translation>Powiadomienie</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.ui" line="151"/>
+        <source>Full screen window</source>
+        <translation>Pełnoekranowe okno</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.cpp" line="29"/>
+        <location filename="createeventdialog.cpp" line="38"/>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.cpp" line="29"/>
+        <source>Title connot be empty</source>
+        <translation>Tytuł nie może być pusty</translation>
+    </message>
+    <message>
+        <location filename="createeventdialog.cpp" line="38"/>
+        <source>Time must be greater then one second</source>
+        <translation>Czas musi być większy niż zero sekund</translation>
+    </message>
+</context>
+<context>
+    <name>EditEventDialog</name>
+    <message>
+        <location filename="editeventdialog.ui" line="14"/>
+        <source>Edit</source>
+        <translation>Edytuj</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="25"/>
+        <source>Title</source>
+        <translation>Tytuł</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="35"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="45"/>
+        <source>Repeat Every</source>
+        <translation>Powtarzaj co</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="54"/>
+        <source>Hours</source>
+        <translation>Godziny</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="64"/>
+        <source>Minutes</source>
+        <translation>Minuty</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="74"/>
+        <source>Seconds</source>
+        <translation>Sekundy</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="86"/>
+        <source>Time Date</source>
+        <translation>Data czas</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="96"/>
+        <source>Show as</source>
+        <translation>Pokaż jako</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="104"/>
+        <source>Notification</source>
+        <translation>Powiadomienie</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.ui" line="109"/>
+        <source>Full screen window</source>
+        <translation>Pełnoekranowe okno</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.cpp" line="55"/>
+        <location filename="editeventdialog.cpp" line="63"/>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.cpp" line="55"/>
+        <source>Title connot be empty</source>
+        <translation>Tytuł nie może być pusty</translation>
+    </message>
+    <message>
+        <location filename="editeventdialog.cpp" line="63"/>
+        <source>Time must be greater then one second</source>
+        <translation>Czas musi być większy niz zero sekund</translation>
+    </message>
+</context>
+<context>
+    <name>EventManager</name>
+    <message>
+        <location filename="eventmanager.cpp" line="17"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="eventmanager.cpp" line="19"/>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+</context>
+<context>
+    <name>EventWidget</name>
+    <message>
+        <location filename="eventwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="26"/>
+        <source>title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="33"/>
+        <source>date</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="40"/>
+        <source>enabled</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="49"/>
+        <source>description</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="60"/>
+        <source>Enable</source>
+        <translation>Włącz</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="70"/>
+        <source>Disable</source>
+        <translation>Wyłącz</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="80"/>
+        <source>Edit</source>
+        <translation>Edytuj</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.ui" line="90"/>
+        <source>Delete</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.cpp" line="30"/>
+        <source>Enabled</source>
+        <translation>Włączone</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.cpp" line="30"/>
+        <source>Disabled</source>
+        <translation>Wyłączone</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.cpp" line="46"/>
+        <source>Are you sure?</source>
+        <translation>Czy na pewno?</translation>
+    </message>
+    <message>
+        <location filename="eventwidget.cpp" line="46"/>
+        <source>Delete %1?</source>
+        <translation>Usunąć %1?</translation>
+    </message>
+</context>
+<context>
+    <name>FullScreenDialog</name>
+    <message>
+        <location filename="fullscreendialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="fullscreendialog.ui" line="23"/>
+        <location filename="fullscreendialog.ui" line="30"/>
+        <location filename="fullscreendialog.ui" line="37"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="fullscreendialog.ui" line="44"/>
+        <source>Ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="fullscreendialog.ui" line="51"/>
+        <source>Disable</source>
+        <translation>Wyłącz</translation>
+    </message>
+    <message>
+        <location filename="fullscreendialog.cpp" line="37"/>
+        <source>Every %1 seconds</source>
+        <translation>Co %1 sekund</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>PCS Reminder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="30"/>
+        <source>Add reminder</source>
+        <translation>Dodaj przypomnienie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="69"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <source>Edit</source>
+        <translation>Edycja</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <source>View</source>
+        <translation>Widok</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <source>About</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>About PCS Reminder</source>
+        <translation>O PCS Reminder</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>About Qt</source>
+        <translation>O Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>Refresh</source>
+        <translation>Odśwież</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>Disable all</source>
+        <translation>Wyłącz wszystkie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>Enable all</source>
+        <translation>Włącz wszystkie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="138"/>
+        <source>Exit</source>
+        <translation>Wyjdź</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="141"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <source>Close Window</source>
+        <translation>Zamknij okno</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Reminder&lt;/h3&gt;&lt;p&gt;PCS Reminder is a simple reminder application. It is a part of Pinecone Snake project.&lt;/p&gt;&lt;p&gt;Version: %1&lt;/p&gt;&lt;p&gt;License: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Reminder&lt;/h3&gt;&lt;p&gt;PCS Reminder jest prostą aplikacją do wyświetlania przypomnień. Jest częścią projektu Pinecone Snake.&lt;/p&gt;&lt;p&gt;Wersja: %1&lt;/p&gt;&lt;p&gt;Licencja: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>RepeatedEventWidget</name>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="26"/>
+        <source>title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="33"/>
+        <source>repear every</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="40"/>
+        <source>enabled</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="49"/>
+        <source>description</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="60"/>
+        <source>Enable</source>
+        <translation>Włącz</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="70"/>
+        <source>Disable</source>
+        <translation>Wyłącz</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="80"/>
+        <source>Edit</source>
+        <translation>Edytuj</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.ui" line="90"/>
+        <source>Delete</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.cpp" line="30"/>
+        <source>Enabled</source>
+        <translation>Włączone</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.cpp" line="30"/>
+        <source>Disabled</source>
+        <translation>Wyłączone</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.cpp" line="46"/>
+        <source>Are you sure?</source>
+        <translation>Czy na pewno?</translation>
+    </message>
+    <message>
+        <location filename="repeatedeventwidget.cpp" line="46"/>
+        <source>Delete %1?</source>
+        <translation>Usunąć %1?</translation>
+    </message>
+</context>
+</TS>
