35bfd9f83d201f6522999ba0d6c7290361bd2392
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-07-25 14:03:26 +0200

    Added translation

--- pcsnotebook/src/pcsnotebook_pl_PL.ts
+++ pcsnotebook/src/pcsnotebook_pl_PL.ts
@@ -45,174 +45,300 @@
         <translation>Plik</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="77"/>
+        <location filename="mainwindow.ui" line="82"/>
         <source>Edit</source>
         <translation>Edycja</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="89"/>
+        <location filename="mainwindow.ui" line="86"/>
+        <source>Change characters size</source>
+        <translation>Zmień rozmiar znaków</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="110"/>
         <source>&amp;View</source>
         <translation>&amp;Widok</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="96"/>
+        <location filename="mainwindow.ui" line="123"/>
         <source>About</source>
         <translation>Pomoc</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="111"/>
+        <location filename="mainwindow.ui" line="138"/>
         <source>&amp;About Notebook</source>
         <translation>O &amp;Notebook</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="119"/>
+        <location filename="mainwindow.ui" line="146"/>
         <source>About &amp;Qt</source>
         <translation>O &amp;Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="127"/>
+        <location filename="mainwindow.ui" line="154"/>
         <source>&amp;New</source>
         <translation>&amp;Nowa</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="130"/>
+        <location filename="mainwindow.ui" line="157"/>
         <source>Ctrl+N</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="138"/>
+        <location filename="mainwindow.ui" line="165"/>
         <source>&amp;Save</source>
         <translation>&amp;Zapisz</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="141"/>
+        <location filename="mainwindow.ui" line="168"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="149"/>
+        <location filename="mainwindow.ui" line="176"/>
         <source>Save &amp;As</source>
         <translation>Zapisz &amp;Jako</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="152"/>
+        <location filename="mainwindow.ui" line="179"/>
         <source>Ctrl+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="160"/>
+        <location filename="mainwindow.ui" line="187"/>
         <source>&amp;Exit</source>
         <translation>&amp;Zakończ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="163"/>
+        <location filename="mainwindow.ui" line="190"/>
         <source>Ctrl+Q</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="171"/>
+        <location filename="mainwindow.ui" line="198"/>
         <source>&amp;Undo</source>
         <translation>&amp;Cofnij</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="174"/>
+        <location filename="mainwindow.ui" line="201"/>
         <source>Ctrl+Z</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="182"/>
+        <location filename="mainwindow.ui" line="209"/>
         <source>&amp;Redo</source>
         <translation>&amp;Przywróć</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="185"/>
+        <location filename="mainwindow.ui" line="212"/>
         <source>Ctrl+Shift+Z</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="193"/>
+        <location filename="mainwindow.ui" line="220"/>
         <source>&amp;Cut</source>
         <translation>&amp;Wytnij</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="196"/>
+        <location filename="mainwindow.ui" line="223"/>
         <source>Ctrl+X</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="204"/>
+        <location filename="mainwindow.ui" line="231"/>
         <source>C&amp;opy</source>
         <translation>&amp;Kopiuj</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="207"/>
+        <location filename="mainwindow.ui" line="234"/>
         <source>Ctrl+C</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="215"/>
+        <location filename="mainwindow.ui" line="242"/>
         <source>&amp;Paste</source>
         <translation>W&amp;klej</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="218"/>
+        <location filename="mainwindow.ui" line="245"/>
         <source>Ctrl+V</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="226"/>
+        <location filename="mainwindow.ui" line="253"/>
         <source>&amp;Select All</source>
         <translation>Zaznacz &amp;Wszystko</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="229"/>
+        <location filename="mainwindow.ui" line="256"/>
         <source>Ctrl+A</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="237"/>
+        <location filename="mainwindow.ui" line="264"/>
         <source>&amp;Refresh</source>
         <translation>&amp;Odświerz</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="240"/>
+        <location filename="mainwindow.ui" line="267"/>
         <source>Ctrl+R</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="248"/>
+        <location filename="mainwindow.ui" line="275"/>
         <source>&amp;Close Tab</source>
         <translation>&amp;Zamknij Kartę</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="251"/>
+        <location filename="mainwindow.ui" line="278"/>
         <source>Ctrl+W</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="32"/>
+        <location filename="mainwindow.ui" line="283"/>
+        <source>Increase font Size</source>
+        <translation>Zwiększ rozmiar czcionki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="288"/>
+        <source>Decrease font size</source>
+        <translation>Zmniejsz rozmiar czcionki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="293"/>
+        <source>Set font size</source>
+        <translation>Ustaw rozmiar czcionki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="298"/>
+        <source>Reset font size</source>
+        <translation>Zresetuj rozmiar czcionki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="303"/>
+        <source>Merge lines</source>
+        <translation>Połącz linijki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="308"/>
+        <source>make selected small</source>
+        <translation>zamień zaznaczone na małe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="313"/>
+        <source>MAKE SELECTED CAPITAL</source>
+        <translation>ZAMIEŃ ZAZNACZONE NA DUŻE</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="318"/>
+        <source>Make selected sentence capital</source>
+        <translation>Duża litera na początku zdania</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="323"/>
+        <source>Make Every Selected Word Capital</source>
+        <translation>Każde słowo z dużej litery</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="331"/>
+        <source>Lines wrap</source>
+        <translation>Zawijaj linie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="339"/>
+        <source>Overwrite mode</source>
+        <translation>Tryb zastępowania</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="347"/>
+        <source>Read only</source>
+        <translation>Tylko do odczytu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="355"/>
+        <source>Print</source>
+        <translation>Drukuj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="358"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="366"/>
+        <source>Close All</source>
+        <translation>Zamknij wszystko</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="374"/>
+        <source>Close All but this</source>
+        <translation>Zamknij wszystko oprócz tego</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="382"/>
+        <location filename="mainwindow.cpp" line="272"/>
+        <source>Paste from file</source>
+        <translation>Wklej z pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
         <source>About Qt</source>
         <translation>O Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="160"/>
+        <location filename="mainwindow.cpp" line="194"/>
         <source>Delete note?</source>
         <translation>Usunąć notatkę?</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="160"/>
+        <location filename="mainwindow.cpp" line="194"/>
         <source>Do you want to delete %1?</source>
         <translation>Czy na pewno chcesz usunąć %1?</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="178"/>
+        <location filename="mainwindow.cpp" line="212"/>
         <source>About Notebook</source>
         <translation>O Notebook</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="178"/>
+        <location filename="mainwindow.cpp" line="212"/>
+        <source>
+    &lt;html&gt;
+        &lt;body&gt;
+            &lt;h3&gt;PCS Notebook&lt;/h3&gt;
+            &lt;p&gt;PCS Notebook is an application for taking notes. It is a part of &lt;a href=&quot;%1&quot;&gt;Pinecone Snake&lt;/a&gt; project.&lt;/p&gt;
+            &lt;p&gt;Version: %2&lt;/p&gt;
+            &lt;p&gt;License: &lt;a href=&quot;%3&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;
+        &lt;/body&gt;
+    &lt;/html&gt;</source>
+        <translation>
+    &lt;html&gt;
+        &lt;body&gt;
+            &lt;h3&gt;PCS Notebook&lt;/h3&gt;
+            &lt;p&gt;PCS Notebook jest aplikacją do robienia notatek. Jest częścią projektu &lt;a href=&quot;%1&quot;&gt;Pinecone Snake&lt;/a&gt;.&lt;/p&gt;
+            &lt;p&gt;Wersja: %2&lt;/p&gt;
+            &lt;p&gt;Licencja: &lt;a href=&quot;%3&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;
+        &lt;/body&gt;
+    &lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="333"/>
+        <source>Enter font size</source>
+        <translation>Wprowadź rozmiar czcionki</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="338"/>
+        <source>Ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="339"/>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
         <source>&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Notebook&lt;/h3&gt;&lt;p&gt;PCS Notebook is an application for taking notes. It is a part of Pinecone Snake project.&lt;/p&gt;&lt;p&gt;Version: %1&lt;/p&gt;&lt;p&gt;License: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Notebook&lt;/h3&gt;&lt;p&gt;PCS Notebook jest aplikacją do robienia notatek. Jest częścią projektu Pinecone Snake.&lt;/p&gt;&lt;p&gt;Wersja: %1&lt;/p&gt;&lt;p&gt;Licencja: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="vanished">&lt;html&gt;&lt;body&gt;&lt;h3&gt;PCS Notebook&lt;/h3&gt;&lt;p&gt;PCS Notebook jest aplikacją do robienia notatek. Jest częścią projektu Pinecone Snake.&lt;/p&gt;&lt;p&gt;Wersja: %1&lt;/p&gt;&lt;p&gt;Licencja: &lt;a href=&quot;%2&quot;&gt;GPL 3&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -272,7 +398,7 @@
     </message>
     <message>
         <location filename="writer.ui" line="38"/>
-        <location filename="writer.cpp" line="68"/>
+        <location filename="writer.cpp" line="103"/>
         <source>Save As</source>
         <translation>Zapisz Jako</translation>
     </message>
