a7d9f5306c90fb5df1f12cd7d3e0b8d4260f21bb
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-09-07 15:59:15 +0200

    Implemented closing tabs actions

--- pcspdfviewer/src/mainwindow.cpp
+++ pcspdfviewer/src/mainwindow.cpp
@@ -12,7 +12,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     connect(ui->tabWidget, &QTabWidget::tabCloseRequested, this, &MainWindow::closeTab);
     connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::open);
-
+    connect(ui->actionClose, &QAction::triggered, this, [&]{closeTab(ui->tabWidget->currentIndex());});
+    connect(ui->actionClose_All, &QAction::triggered, this, [&]{while(ui->tabWidget->count() > 0)closeTab(ui->tabWidget->currentIndex());});
+    connect(ui->actionClose_All_But_This, &QAction::triggered, this, [&]{for(int i=ui->tabWidget->count()-1; i >= 0;--i)if(i != ui->tabWidget->currentIndex())closeTab(i);});
+    connect(ui->actionExit, &QAction::triggered, qApp, &QApplication::closeAllWindows);
     ui->splitter->setStretchFactor(1, 1);
 }
 
@@ -47,7 +50,7 @@ void MainWindow::open()
             QPdfDocument* document = new QPdfDocument(pdfView);
             document->load(path);
             pdfView->setDocument(document);
-            ui->tabWidget->addTab(pdfView, path);
+            ui->tabWidget->addTab(pdfView, document->metaData(QPdfDocument::MetaDataField::Title).toString());
         }
     }
 }
